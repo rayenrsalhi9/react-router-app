3740c25930adc558208388066b2d58a75b1f8d48
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-08-27 13:57:49 +0100

    refactor(host-van): move data fetching to layout component
    The data fetching logic was moved from HostVanDetails to HostVanLayout to better organize the component hierarchy and avoid duplicate API calls. HostVanDetails is now a pure presentational component that receives data via props.

--- src/components/HostVanLayout.jsx
+++ src/components/HostVanLayout.jsx
@@ -1,17 +1,36 @@
-import { Link, Outlet } from 'react-router-dom'
+import React from 'react'
+import { Outlet, useParams } from 'react-router-dom'
 import HostVanDetails from '../pages/host/HostVanDetails'
 import HostVanHeader from './HostVanHeader'
-import { FaArrowLeft } from 'react-icons/fa'
 import BackLink from './BackLink'
 
 export default function HostVanLayout() {
+
+  const [vanDetails, setVanDetails] = React.useState(null)
+
+  const {id} = useParams()
+
+  React.useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then(res => res.json())
+      .then(data => setVanDetails(data.vans[0]))
+      .catch(err => console.log(err))
+  }, [id])
+
   return (
     <section className="host-van-details">
         <BackLink />
         <div className="host-van-details-wrapper">
-            <HostVanDetails />
-            <HostVanHeader />
-            <Outlet />
+            {
+              vanDetails ?
+              (
+                <>
+                  <HostVanDetails vanDetails={vanDetails} />
+                  <HostVanHeader />
+                  <Outlet/>
+                </>
+              ) : <h2>Loading...</h2>
+            }
         </div>
     </section>
   )
--- src/pages/host/HostVanDetails.jsx
+++ src/pages/host/HostVanDetails.jsx
@@ -1,20 +1,6 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+export default function HostVanDetails({vanDetails}) {
 
-export default function HostVanDetails() {
-
-  const [vanDetails, setVanDetails] = React.useState(null)
-
-  const {id} = useParams()
-
-  React.useEffect(() => {
-    fetch(`/api/host/vans/${id}`)
-      .then(res => res.json())
-      .then(data => setVanDetails(data.vans[0]))
-      .catch(err => console.log(err))
-  }, [id])
-
-  const vanElement = vanDetails ? (
+  return(
     <div className="host-van-details-card">
       <img src={vanDetails.imageUrl} alt={`Image of ${vanDetails.name}`} />
       <div className="host-van-details-card-info">
@@ -25,7 +11,5 @@ export default function HostVanDetails() {
         <p className='van-price'>${vanDetails.price}<span>/day</span></p>
       </div>
     </div>
-  ) : <h2>Loading...</h2>
-
-  return vanElement
+  )
 }
