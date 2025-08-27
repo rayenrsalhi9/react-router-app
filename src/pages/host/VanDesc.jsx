1f4fe8fa3239e8efa06dcf0906a183ef12bc1e43
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-08-27 14:32:39 +0100

    feat(host/VanDesc): enhance van details display with additional fields
    Add name, category, and visibility fields to the van description component.
    Adjust CSS margins for better spacing between description elements.

--- src/pages/host/VanDesc.jsx
+++ src/pages/host/VanDesc.jsx
@@ -2,12 +2,25 @@ import { useOutletContext } from "react-router-dom"
 
 export default function VanDesc() {
 
-  const { description } = useOutletContext()
+  const { name, type, description } = useOutletContext()
 
   return (
     <div className="host-van-description">
       <p>
+        <b>Name: </b>
+        { name }
+      </p>
+      <p>
+        <b>Category: </b>
+        { type[0].toUpperCase() + type.slice(1) }
+      </p>
+      <p>
+        <b>Description: </b>
         { description }
+      </p>
+      <p>
+        <b>Visibility: </b>
+        Public
       </p>
     </div>
   )
