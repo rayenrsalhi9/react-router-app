9965ccbab1ece98b213452c29b21a0b61fa46fb9
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-08-28 14:38:10 +0100

    refactor(VansFilters): extract filter logic into reusable function
    The handleNewFilter function was added to centralize the logic for updating and clearing search parameters, making the code more maintainable and reducing duplication.

--- src/components/VansFilters.jsx
+++ src/components/VansFilters.jsx
@@ -1,12 +1,21 @@
 export default function VansFilters({ typeFilter, setSearchParams }) {
+
+    function handleNewFilter(key, value) {
+        setSearchParams(prev => {
+            if (value === null) prev.delete(key)
+            else prev.set(key, value)
+            return prev
+        })
+    }
+
     return(
         <nav className="vans-filters">
-            <button className={'filter simple' + (typeFilter === 'simple' ? ' active' : '')} onClick={() => setSearchParams({type: 'simple'})}>Simple</button>
-            <button className={'filter rugged' + (typeFilter === 'rugged' ? ' active' : '')} onClick={() => setSearchParams({type: 'rugged'})}>Rugged</button>
-            <button className={'filter luxury' + (typeFilter === 'luxury' ? ' active' : '')} onClick={() => setSearchParams({type: 'luxury'})}>Luxury</button>
+            <button className={'filter simple' + (typeFilter === 'simple' ? ' active' : '')} onClick={() => handleNewFilter('type', 'simple')}>Simple</button>
+            <button className={'filter rugged' + (typeFilter === 'rugged' ? ' active' : '')} onClick={() => handleNewFilter('type', 'rugged')}>Rugged</button>
+            <button className={'filter luxury' + (typeFilter === 'luxury' ? ' active' : '')} onClick={() => handleNewFilter('type', 'luxury')}>Luxury</button>
             {
                 typeFilter
-                ? <button className='clear-filters-link' onClick={() => setSearchParams({})}>Clear filters</button>
+                ? <button className='clear-filters-link' onClick={() => handleNewFilter('type', null)}>Clear filters</button>
                 : null
             }
         </nav>
