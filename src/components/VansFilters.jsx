3827e563eea3c35e40996cbedff19962e588e8b2
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-08-28 14:22:55 +0100

    feat(ui): add active state styling to filter buttons
    Add cursor pointer to return link and implement active state styling for filter buttons to improve user experience

--- src/components/VansFilters.jsx
+++ src/components/VansFilters.jsx
@@ -1,9 +1,9 @@
 export default function VansFilters({ typeFilter, setSearchParams }) {
     return(
         <nav className="vans-filters">
-            <button className='filter simple' onClick={() => setSearchParams({type: 'simple'})}>Simple</button>
-            <button className='filter rugged' onClick={() => setSearchParams({type: 'rugged'})}>Rugged</button>
-            <button className='filter luxury' onClick={() => setSearchParams({type: 'luxury'})}>Luxury</button>
+            <button className={'filter simple' + (typeFilter === 'simple' ? ' active' : '')} onClick={() => setSearchParams({type: 'simple'})}>Simple</button>
+            <button className={'filter rugged' + (typeFilter === 'rugged' ? ' active' : '')} onClick={() => setSearchParams({type: 'rugged'})}>Rugged</button>
+            <button className={'filter luxury' + (typeFilter === 'luxury' ? ' active' : '')} onClick={() => setSearchParams({type: 'luxury'})}>Luxury</button>
             {
                 typeFilter
                 ? <button className='clear-filters-link' onClick={() => setSearchParams({})}>Clear filters</button>
